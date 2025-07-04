11ba654da1d3aab10b0cd82b397cf3c9d9e4aa1e
SaksitNaisoonthorn <pondzpspln@gmail.com>
2025-07-04 11:10:48 +0700

    Update index.js

--- timesheet-backend/src/index.js
+++ timesheet-backend/src/index.js
@@ -1,23 +1,23 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 
-const profileRoutes = require('./routes/profileRoutes');
-const authRoutes = require('./routes/authRoutes');
-const userRoutes = require('./routes/userRoutes');
-const timesheetRoutes = require('./routes/timesheetRoutes');
-const adminRoutes = require('./routes/adminRoutes'); // ✅ ต้องเพิ่ม
+const profileRoutes = require("./routes/profileRoutes");
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+const timesheetRoutes = require("./routes/timesheetRoutes");
+const adminRoutes = require("./routes/adminRoutes"); // ✅ ต้องเพิ่ม
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/timesheets', timesheetRoutes);
-app.use('/api/profile', profileRoutes);
-app.use('/api/admin', adminRoutes); // ✅ เพิ่มตรงนี้ให้ถูก path
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/timesheets", timesheetRoutes);
+app.use("/api/profile", profileRoutes);
+app.use("/api/admin", adminRoutes); // ✅ เพิ่มตรงนี้ให้ถูก path
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
